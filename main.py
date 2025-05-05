7b3c9d94fa2b2fa04461b04c5a443905d3514156
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-05-05 11:34:58 +0100

    Added line class

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from window import *
+from point import *
+from line import *
 
 def main():
     win = Window(800, 600)
+
+    top_left = Point(10,10)
+    top_right = Point(100,10)
+    bot_left = Point(10,100)
+    bot_right = Point(100,100)
+
+    top = Line(top_left, top_right)
+    left = Line(top_left, bot_left)
+    right = Line(top_right, bot_right)
+    bottom = Line(bot_left, bot_right)
+
+    win.draw_line(top, "black")
+    win.draw_line(left, "red")
+    win.draw_line(right, "blue")
+    win.draw_line(bottom, "green")
+
     win.wait_for_close()
 
 main()
